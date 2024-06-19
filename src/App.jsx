4fae42d31eed6224a7f99cb4d534129eac5cd303
sd025 <shubham.dhumal452@gmail.com>
2024-06-19 23:02:19 +0530

    toggle chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import BarChart from "./BarChart";
 
 export default function App() {
+    const [showChart, setShowChart] = useState(false)
+
     return (
-    <main>yep</main>
+    <main>
+        <button>Toggle Chart</button>
+        {showChart ? 
+        <BarChart /> : null}
+    </main>
     )
 }
