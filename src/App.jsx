27ca8c10ad3fae3f236a1d4f04113714288754c2
sd025 <shubham.dhumal452@gmail.com>
2024-06-20 11:24:36 +0530

    bar config and datajs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import BarChart from "./BarChart";
-import { CHART_DATA } from "./data";
 
 export default function App() {
     const [showChart, setShowChart] = useState(false)
 
     return (
     <main>
-        <button onClick={() => setShowChart(prev => !prev)}>Toggle Chart</button>
+        <button>Toggle Chart</button>
         {showChart ? 
-        <BarChart data={CHART_DATA} /> : null}
+        <BarChart /> : null}
     </main>
     )
 }
